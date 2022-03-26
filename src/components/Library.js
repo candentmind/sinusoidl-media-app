50df4aa318442122b8bde56be32d8fac96b9095a
CandentMind <efe.emma@gmail.com>
2022-03-26 13:19:30 +0100

    Add backdrop to Library component Also enable setLibraryVisibility functionality on click

--- src/components/Library.js
+++ src/components/Library.js
@@ -10,26 +10,32 @@ const Library = ({
   setLibraryVisibility,
 }) => {
   return (
-    <div className={`library ${libraryIsVisible ? "active-library" : ""}`}>
-      <div className="library-songs">
-        {songs.map((song, index) => (
-          <Song
-            song={song}
-            key={song.id}
-            isPlaying={isPlaying}
-            // audioRef={props.audioRef}
-            className={`song-container song-container--horizontal row ${
-              index === currentSongIndex ? "selected" : ""
-            }`}
-            currentSong={song}
-            onSelectSong={() => {
-              onChangeSong({ direction: "SET_AT_POSITION", index });
-              if (libraryIsVisible) setLibraryVisibility();
-            }}
-          />
-        ))}
+    <>
+      <section
+        className={`${libraryIsVisible ? "library-backdrop" : ""}`}
+        onClick={setLibraryVisibility}
+      ></section>
+      <div className={`library ${libraryIsVisible ? "active-library" : ""}`}>
+        <div className="library-songs">
+          {songs.map((song, index) => (
+            <Song
+              song={song}
+              key={song.id}
+              isPlaying={isPlaying}
+              // audioRef={props.audioRef}
+              className={`song-container song-container--horizontal row ${
+                index === currentSongIndex ? "selected" : ""
+              }`}
+              currentSong={song}
+              onSelectSong={() => {
+                onChangeSong({ direction: "SET_AT_POSITION", index });
+                if (libraryIsVisible) setLibraryVisibility();
+              }}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
